30bb3dffcc0206b22fcce944231c6da478e1f446
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-02-10 19:37:45 +0900

    login sign in page updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,6 +19,8 @@
 		<link href="{{ asset('assets/admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
 		<!-- Head js -->
 		<script src="{{ asset('assets/admin/assets/js/head.js')}}"></script>
+        <!-- font awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     </head>
 
@@ -35,29 +37,30 @@
                             <div class="auth-logo">
                                 <a href="index.html" class="logo logo-dark text-center">
                                     <span class="logo-lg">
-                                        <img src="assets/images/logo-dark.png" alt="" height="22">
+                                        <img src="{{ asset('./assets/admin/assets/images/logo-dark.png') }}" alt="" height="22">
                                     </span>
                                 </a>
 
                                 <a href="index.html" class="logo logo-light text-center">
                                     <span class="logo-lg">
-                                        <img src="assets/images/logo-light.png" alt="" height="22">
+                                        
+                                        <img src="{{ asset('./assets/admin/assets/images/logo-light.png') }}" alt="" height="22">
                                     </span>
                                 </a>
                             </div>
                         </div>
 
                         <!-- title-->
-                        <h4 class="mt-0">Sign In</h4>
-                        <p class="text-muted mb-4">Enter your email address and password to access account.</p>
+                        <h4 class="mt-0 display-1 fw-bold">Sign In</h4>
+                        <p class="text-muted mb-4">Masukan alamat email dan kata sandi Anda untuk mengakses.</p>
 
                         <!-- form -->
                         <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="mb-3">
-                                <label for="emailaddress" class="form-label">Email address</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <label for="emailaddress" class="form-label">Alamat Email</label>
+                                <input id="email" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Alamat Email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -66,10 +69,10 @@
                                 @enderror
                             </div>
                             <div class="mb-3">
-                                <a href="auth-recoverpw-2.html" class="text-muted float-end"><small>Forgot your password?</small></a>
-                                <label for="password" class="form-label">Password</label>
+                                <a href="auth-recoverpw-2.html" class="text-muted float-end"><small>Lupa kata sandi Anda?</small></a>
+                                <label for="password" class="form-label">Kata Sandi</label>
                                 <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    <input type="password" id="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Kata Sandi">
                                     <div class="input-group-text" data-password="false">
                                         <span class="password-eye"></span>
                                     </div>
@@ -93,7 +96,7 @@
                             </div>
                         </div>
                             <div class="text-center d-grid">
-                                <button class="btn btn-primary" type="submit">Log In </button>
+                                <button class="btn btn-lg btn-primary text-uppercase" type="submit"> <i class="fa-solid fa-sign-in me-1"></i> Log In </button>
                             </div>
 
                         </form>
@@ -101,7 +104,7 @@
 
                         <!-- Footer-->
                         <footer class="footer footer-alt">
-                            <p class="text-muted">Don't have an account? <a href="auth-register-2.html" class="text-muted ms-1"><b>Sign Up</b></a></p>
+                            <p class="text-muted">Belum punya akun? <a href="#hubungi-admin" class="text-muted ms-1"><b><i class="fa-brands fa-whatsapp"></i> Hubungi Admin</b></a></p>
                         </footer>
 
                     </div> <!-- end .card-body -->
@@ -112,12 +115,10 @@
             <!-- Auth fluid right content -->
             <div class="auth-fluid-right text-center">
                 <div class="auth-user-testimonial">
-                    <h2 class="mb-3 text-white">I love the color!</h2>
-                    <p class="lead"><i class="mdi mdi-format-quote-open"></i> I've been using your theme from the previous developer for our web app, once I knew new version is out, I immediately bought with no hesitation. Great themes, good documentation with lots of customization available and sample app that really fit our need. <i class="mdi mdi-format-quote-close"></i>
-                    </p>
-                    <h5 class="text-white">
-                        - Fadlisaad (Ubold Admin User)
-                    </h5>
+                    <img src="{{ asset('./assets/admin/assets/images/lanny-jaya/logo-kab-lanny-jaya.png') }}" alt="Logo Kabupaten Lanny Jaya" class="img img-fluid py-lg-3" style="width: 300px;">
+                    <h1 class="display-1 fw-bold text-white">E-LPPD</h1>
+                    <p class="fw-bold h2 text-white text-uppercase">Sistem Informasi laporan Penyelengaraan Pemerintah Daerah</p>
+                    <p class="fw-bold h2 text-white text-uppercase">Kabupaten Lanny Jaya</p>
                 </div> <!-- end auth-user-testimonial-->
             </div>
             <!-- end Auth fluid right content -->
