7cf774d8e027089fa986f9fb48cf8f87fb949ec6
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-03-18 01:53:22 +0900

    update s title auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title> s SILPPD Kabupaten Lanny Jaya</title>
+        <title> SILPPD Kabupaten Lanny Jaya</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
