397ae8c6bd14b422774bfacc373d8be0fb79f2f2
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-03-29 15:39:05 +0900

    (+) app environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        } elseif(App::environment('local')){
+            URL::forceScheme('http');
+        }
+
     }
 }
