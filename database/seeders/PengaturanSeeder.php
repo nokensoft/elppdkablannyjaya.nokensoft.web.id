ab15d972b047ccb44ab31de116fe6336610a6d03
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-05-08 00:23:39 +0900

    UPDTED: pelaporan

--- database/seeders/PengaturanSeeder.php
+++ database/seeders/PengaturanSeeder.php
@@ -14,13 +14,13 @@ class PengaturanSeeder extends Seeder
             'judul_situs'           => 'SILPPD',
             'deskripsi_situs'       => 'Sistem Informasi Laporan Penyelenggaraan Pengelolaan Daerah',
 
-            'logo_sm_dark'          => 'assets/images/logo/logo.png',
-            'logo_lg_dark'          => 'assets/images/logo/logo.png',
+            'logo_sm_dark'          => 'logo-small-dark.png',
+            'logo_lg_dark'          => 'logo-large-dark.png',
             
-            'logo_sm_light'         => 'assets/images/logo/logo.png',
-            'logo_lg_light'         => 'assets/images/logo/logo.png',
+            'logo_sm_light'         => 'logo-small-light.png',
+            'logo_lg_light'         => 'logo-large-light.png',
 
-            'favicon'               => 'assets/images/logo/favicon.png',
+            'favicon'               => 'favicon.png',
             'termsconditions'       => '',
             'footerinformation'     => '2023 © SISTEM INFORMASI LAPORAN PENYELENGGARAAN PEMERINTAH DAERAH, KABUPATEN LANNY JAYA.',
             'tentang_aplikasi'      => '<p>Aplikas SIPPID Kabupaten Lanny Jaya dibuat untuk mempermudah Pemerintah Kabupaten Lanny Jaya dalam mengumpulan dan mengelola LPPD secara online.</p> <p>Sistem ini digunakan oleh Pemerintah Daerah Kabupaten Lanny Jaya dengan sistem basis data (database) yang terpusat agar memudahkan petugas dan pejabat dalam mengelola data. Dengan sistem basis data secara online dan terpusat, membuat proses penyampaikan laporan kinerja lebih efisien dan konsisten. Selain itu, data tersebut juga mudah diakses dari mana saja dan kapan saja.</p>',
