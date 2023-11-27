7fcd11f3ce475899e129cbc49694c659adc6a666
Johan Nasendi <57840332+johan-nasendi@users.noreply.github.com>
2023-11-28 02:23:47 +0800

    Update GeospatialController.php

--- app/Http/Controllers/Api/GeospatialController.php
+++ app/Http/Controllers/Api/GeospatialController.php
@@ -9,14 +9,15 @@ use App\Http\Controllers\Controller;
 class GeospatialController extends Controller
 {
 
+
     public function getDistricts()
     {
         $query = "
-            SELECT d.distrik_id, d.nama_distrik, ST_AsGeoJSON(d.peta_distrik) AS geojson, SUM(de.jumlah_penduduk) AS total_penduduk
-            FROM distriks d
-            LEFT JOIN desas de ON d.distrik_id = de.distrik_id
-            GROUP BY d.distrik_id, d.nama_distrik, d.peta_distrik
-        ";
+        SELECT d.id, d.nama_distrik, ST_AsGeoJSON(d.peta_distrik) AS geojson, SUM(de.jumlah_penduduk) AS total_penduduk
+        FROM distriks d
+        LEFT JOIN desas de ON d.id = de.id
+        GROUP BY d.id, d.nama_distrik, d.peta_distrik";
+
         $districts = DB::select($query);
         return response()->json($this->createGeoJSON($districts, true));
     }
@@ -24,7 +25,7 @@ class GeospatialController extends Controller
     public function getVillages(Request $request)
     {
         $districtId = $request->query('districtId');
-        $query = "SELECT id, distrik_id, nama_desa,nama_kepala_desa,jumlah_penduduk, ST_AsGeoJSON(peta_desa) AS geojson FROM desas";
+        $query = "SELECT id, distrik_id, nama_desa, ST_AsGeoJSON(peta_desa) AS geojson FROM desas";
         if ($districtId) {
             $query .= " WHERE distrik_id = ?";
             $villages = DB::select($query, [$districtId]);
@@ -37,21 +38,17 @@ class GeospatialController extends Controller
     private function createGeoJSON($data, $isDistrict = false)
     {
         $features = array_map(function ($item) use ($isDistrict) {
-            $properties = $isDistrict
-                ?
-                [
-                    'distrik_id' => $item->distrik_id,
-                    'name' => $item->nama_distrik,
-                    'nama_kepala_distrik' => $item->nama_kepala_distrik
-                ]
-                :
-                [
-                    'id' => $item->id,
-                    'distrik_id' => $item->distrik_id,
-                    'name' => $item->nama_desa,
-                    'nama_kepala_desa' => $item->nama_kepala_desa,
-                    'jumlah_penduduk' => $item->jumlah_penduduk,
-                ];
+            $properties = [
+                'name' => $item->nama_distrik ?? $item->nama_desa,
+            ];
+
+            if ($isDistrict) {
+                $properties['id'] = $item->id;
+                $properties['total_penduduk'] = $item->total_penduduk; // Menambahkan total penduduk
+            } else {
+                $properties['id'] = $item->id;
+                $properties['distrik_id'] = $item->distrik_id;
+            }
 
             return [
                 'type' => 'Feature',
@@ -62,6 +59,60 @@ class GeospatialController extends Controller
 
         return ['type' => 'FeatureCollection', 'features' => $features];
     }
+
+     // public function getDistricts()
+    // {
+    //     $query = "
+    //         SELECT d.distrik_id, d.nama_distrik, ST_AsGeoJSON(d.peta_distrik) AS geojson, SUM(de.jumlah_penduduk) AS total_penduduk
+    //         FROM distriks d
+    //         LEFT JOIN desas de ON d.distrik_id = de.distrik_id
+    //         GROUP BY d.distrik_id, d.nama_distrik, d.peta_distrik
+    //     ";
+    //     $districts = DB::select($query);
+    //     return response()->json($this->createGeoJSON($districts, true));
+    // }
+
+    // public function getVillages(Request $request)
+    // {
+    //     $districtId = $request->query('districtId');
+    //     $query = "SELECT id, distrik_id, nama_desa,nama_kepala_desa,jumlah_penduduk, ST_AsGeoJSON(peta_desa) AS geojson FROM desas";
+    //     if ($districtId) {
+    //         $query .= " WHERE distrik_id = ?";
+    //         $villages = DB::select($query, [$districtId]);
+    //     } else {
+    //         $villages = DB::select($query);
+    //     }
+    //     return response()->json($this->createGeoJSON($villages, false));
+    // }
+
+    // private function createGeoJSON($data, $isDistrict = false)
+    // {
+    //     $features = array_map(function ($item) use ($isDistrict) {
+    //         $properties = $isDistrict
+    //             ?
+    //             [
+    //                 'distrik_id' => $item->distrik_id,
+    //                 'name' => $item->nama_distrik,
+    //                 'nama_kepala_distrik' => $item->nama_kepala_distrik
+    //             ]
+    //             :
+    //             [
+    //                 'id' => $item->id,
+    //                 'distrik_id' => $item->distrik_id,
+    //                 'name' => $item->nama_desa,
+    //                 'nama_kepala_desa' => $item->nama_kepala_desa,
+    //                 'jumlah_penduduk' => $item->jumlah_penduduk,
+    //             ];
+
+    //         return [
+    //             'type' => 'Feature',
+    //             'geometry' => json_decode($item->geojson),
+    //             'properties' => $properties,
+    //         ];
+    //     }, $data);
+
+    //     return ['type' => 'FeatureCollection', 'features' => $features];
+    // }
 
     // public function getDistricts()
     // {
